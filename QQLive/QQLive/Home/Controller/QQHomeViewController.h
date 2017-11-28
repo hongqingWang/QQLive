18ffd431a368e8d493a3d4dc17381c54fe91c166
wanghongqing <272338444@qq.com>
2017-11-28 10:24:30 +0800

    初始化代码

---
+++ QQLive/QQLive/Home/Controller/QQHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QQHomeViewController.h
+//  QQLive
+//
+//  Created by Mac on 2017/11/28.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QQHomeViewController : UITableViewController
+
+@end
