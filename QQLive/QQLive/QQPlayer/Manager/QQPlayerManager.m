c40cd286164f7a7bc4bb41fd48cd838369ede7d2
wanghongqing <272338444@qq.com>
2017-11-29 14:07:19 +0800

    删除LMVideoPlayer 完毕
    删除LMVideoPlayer 完毕

---
+++ QQLive/QQLive/QQPlayer/Manager/QQPlayerManager.m
@@ -0,0 +1,13 @@
+//
+//  QQPlayerManager.m
+//  QQLive
+//
+//  Created by Mac on 2017/11/29.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import "QQPlayerManager.h"
+
+@implementation QQPlayerManager
+
+@end
